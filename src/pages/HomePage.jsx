7718f40a42fe405f63860e40726d633cea9ab3fa
RonanKer29 <kervella.ronan@hotmail.com>
2024-05-29 16:08:31 +0200

    restyle background

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,17 +7,15 @@ import photo from "../assets/photo-ronan.jpg";
 const HomePage = () => {
   return (
     <div className="home-page">
-      <div className="animated-container">
-        <div className="image-container">
-          <img src={photo} alt="Ronan Kervella" className="animated-image" />
-        </div>
-        <div className="animated-text-container">
-          <AnimatedText text=" Fullstack Developer." />
-        </div>
-        <div className="presentation">
-          From design to development, I can help you with your web development
-          projects.
-        </div>
+      <div className="image-and-text">
+        <img src={photo} alt="Office" className="office-image" />
+      </div>
+      <div className="animated-text-container">
+        <AnimatedText text=" Fullstack Developer." />
+      </div>
+      <div className="presentation">
+        From design to development, I can help you with your web development
+        projects.
       </div>
     </div>
   );
